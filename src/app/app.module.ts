08c8f6ccf575a6ba0d5cbe0a920d06a13b3075aa
rbshah2712 <34372610+rbshah2712@users.noreply.github.com>
2024-04-16 15:54:09 -0400

    CRUD Operation using angular node.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule // Add HttpClientModule here
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/items/items.ts
@@ -0,0 +1,6 @@
+// items.ts
+export interface Item {
+    _id: string; // Optional if using auto-generated IDs
+    name: string;
+    description: string;
+  }
